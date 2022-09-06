f9ea5520c63f635ae247b170ae57143b7b63919d
OluwoleAjayi <78364159+OluwoleAjayi@users.noreply.github.com>
2022-09-06 01:04:01 +0100

    Adjusted static html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser= require('body-parser');
 
@@ -8,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h2>Page not found</h2>")
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(3000);
