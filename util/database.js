f6c38e5d2cdaa40e96af1edc7865deaa8cc1d8b7
OluwoleAjayi <78364159+OluwoleAjayi@users.noreply.github.com>
2022-09-10 04:11:17 +0100

    working with databases

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'customer_db'
+const sequelize = new Sequelize('customer_db', 'root', '', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+
+module.exports = sequelize;
